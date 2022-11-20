f557f2fcee4b155b953a9fc99a52dba031d9416a
savkunok <117840518+savkunok@users.noreply.github.com>
2022-11-21 01:56:48 +0300

    Create Lab(6).cpp

---
+++ Lab(6).cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class StackException : public std::exception
+{
+public:
+	StackException(const char* Exception) :
+		    std::exception(Exception)
+       {
+       } 
+};
+
+//////////////////////////////////////////
+
+void My_Term()
+{
+	std::cout << "The programm was terminated" << std::endl;
+	exit(-1);
+}
+
+///////////////////////////////////////////
+
+
+
+template <typename T> 
+class Stack
+{
+private:
+   struct Node
+   {
+        T data;
+        Node* next;
+   };
+   Node* root;
+public:
+    Stack() : root (nullptr) {}
+   
+    ~Stack(){
+        while (!empty())
+        {
+            pop();
+        }
+        
+    } 
+    bool empty() const{
+        return root == nullptr;
+    }
+    void push(const T& x){
+        Node* newnode = new Node;
+        newnode->data = x;
+        newnode->next = root;
+        root = newnode;
+    } 
+    T pop(){
+        if(empty()){
+            throw StackException("stack is empty");
+        }
+        Node* delnode = root;
+        T x = delnode->data;
+        root = delnode->next;
+        delete delnode;
+        return x;
+    } 
+    void printStack(){
+        if(empty()){
+            throw StackException("stack is empty"); 
+        }
+        cout<<"stack:" <<endl;
+        while (!empty())
+        {
+            cout<< pop() << endl;
+        }
+    }
+};
+///////////////////////////////////////////////////////////////////////
+template <> class Stack<char*>
+{
+private:
+   struct Node
+   {
+        char* data;
+        Node* next;
+   };
+   Node* root;
+public:
+    Stack() : root (nullptr) {}
+   
+    ~Stack(){
+        while (!empty())
+        {
+            pop();
+        }
+        
+    } 
+    bool empty() const{
+        return root == nullptr;
+    }
+    void push(char* x){
+        Node* newnode = new Node;
+        newnode->data = x;
+        newnode->next = root;
+        root = newnode;
+    } 
+    char* pop(){
+        if(empty()){
+            throw StackException("stack is empty");
+        }
+        Node* delnode = root;
+        char* x = delnode->data;
+        root = delnode->next;
+        delete delnode;
+        return x;
+    } 
+    void printStack(){
+        if(empty()){
+            throw StackException("stack is empty");
+        }
+        cout<<"stack:" <<endl;
+        while (!empty())
+        {
+            cout<< pop() << endl;
+        }
+    }
+
+};
+int main()
+{
+    set_terminate(My_Term);
+
+    char string1[10];
+    char string2[10];
+    char string3[10];
+    cout<<"Enter 1 string:"<<endl;
+    cin >> string1;
+    cout<<"Enter 2 string:"<<endl;
+    cin >> string2;
+    cout<<"Enter 3 string:"<<endl;
+    cin >> string3;
+    Stack<char*> st;
+    st.push(string1);
+    st.push(string2);
+    st.push(string3);
+    st.printStack();
+    
+
+    terminate();
+    return 0;
+}
